ce6951003f57f413c8ac6b705dd75a31bdd9bb2d
Lukas Volgger <lukas.volgger@live.at>
2023-12-28 21:25:09 +0100

    feat: split and modernize contact/contact-form and add validation

---
+++ src/app/components/contact-form/contact-form.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+  contactForm = this._formBuilder.group({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  /**
+   * Retrieves the message for the user and displays it at the frontend
+   * @param controlName The name of the form control for which to retrieve the error message
+   * @returns a string representing the error message or an empty string if no error is present
+   */
+  getErrorMessage(controlName: string) {
+    const control = this.contactForm.get(controlName);
+
+    if (control?.hasError('required')) {
+      return 'This field is required.';
+    }
+
+    if (control?.hasError('minlength')) {
+      return 'You must enter at least 3 characters.';
+    }
+
+    return control?.hasError('email') ? 'Not a valid email address.' : '';
+  }
+}
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })

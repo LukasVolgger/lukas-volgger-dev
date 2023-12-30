0066e2bb9b170ecaa8c8b10e4282635ae57b7dc9
Lukas Volgger <lukas.volgger@live.at>
2023-12-30 12:04:45 +0100

    feat: modernize and fix success component

--- src/app/components/success/success.component.ts
+++ src/app/components/success/success.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-success',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './success.component.html',
   styleUrl: './success.component.scss'
 })

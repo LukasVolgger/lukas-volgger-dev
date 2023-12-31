f142c8af6a5cac252eb40f6faa425a34aaead7cd
Lukas Volgger <lukas.volgger@live.at>
2023-12-31 15:59:35 +0100

    feat: add shooting stars home background

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
 import { RouterModule } from '@angular/router';
 import { ScrollSpyModule } from '@avtest/ng-spy';
+import { StarsBgComponent } from '../stars-bg/stars-bg.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutMeComponent, ProjectsComponent, ContactComponent, RouterModule, ScrollSpyModule],
+  imports: [AboutMeComponent, ProjectsComponent, ContactComponent, RouterModule, ScrollSpyModule, StarsBgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/components/stars-bg/stars-bg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-bg',
+  standalone: true,
+  imports: [],
+  templateUrl: './stars-bg.component.html',
+  styleUrl: './stars-bg.component.scss'
+})
+export class StarsBgComponent {
+
+}

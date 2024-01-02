16a683530b386eb5eee6ea47e3645d3956c2ed44
Lukas Volgger <lukas.volgger@live.at>
2024-01-02 17:36:03 +0100

    chore: refactor and simplify skills component

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -8,5 +8,71 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      caption: 'Java',
+      imageSrc: './../../assets/img/icons/skills/java.svg',
+      imageAlt: 'Java Logo'
+    },
+    {
+      caption: 'TypeScript',
+      imageSrc: './../../assets/img/icons/skills/typescript.svg',
+      imageAlt: 'TypeScript Logo'
+    },
+    {
+      caption: 'JavaScript',
+      imageSrc: './../../assets/img/icons/skills/javascript.svg',
+      imageAlt: 'JavaScript Logo'
+    },
+    {
+      caption: 'Angular',
+      imageSrc: './../../assets/img/icons/skills/angular.svg',
+      imageAlt: 'Angular Logo'
+    },
+    {
+      caption: 'HTML5',
+      imageSrc: './../../assets/img/icons/skills/html5.svg',
+      imageAlt: 'HTML5 Logo'
+    },
+    {
+      caption: 'CSS3',
+      imageSrc: './../../assets/img/icons/skills/css3.svg',
+      imageAlt: 'CSS3 Logo'
+    },
+    {
+      caption: 'SQL',
+      imageSrc: './../../assets/img/icons/skills/sql.svg',
+      imageAlt: 'SQL Logo'
+    },
+    {
+      caption: 'Docker',
+      imageSrc: './../../assets/img/icons/skills/docker.svg',
+      imageAlt: 'Docker Logo'
+    },
+    {
+      caption: 'Git',
+      imageSrc: './../../assets/img/icons/skills/git.svg',
+      imageAlt: 'Git Logo'
+    },
+    {
+      caption: 'REST-API',
+      imageSrc: './../../assets/img/icons/skills/api.svg',
+      imageAlt: 'REST-API Logo'
+    },
+    {
+      caption: 'SCRUM',
+      imageSrc: './../../assets/img/icons/skills/scrum.svg',
+      imageAlt: 'SCRUM Logo'
+    },
+    {
+      caption: 'Material Design',
+      imageSrc: './../../assets/img/icons/skills/material_design.svg',
+      imageAlt: 'Material Design Logo'
+    },
+    {
+      caption: 'Bootstrap',
+      imageSrc: './../../assets/img/icons/skills/bootstrap.svg',
+      imageAlt: 'Bootstrap Logo'
+    },
+  ]
 }

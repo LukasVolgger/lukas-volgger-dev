47047b8c611f5ad1462a694d62550eaef386593b
Lukas Volgger <lukas.volgger@live.at>
2024-01-02 21:20:09 +0100

    fix: remove highlighting nav links on legal information page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ScrollSpyModule, ScrollSpyService } from '@avtest/ng-spy';
 import { NavbarDesktopComponent } from '../navbar-desktop/navbar-desktop.component';
 import { NavbarMobileComponent } from '../navbar-mobile/navbar-mobile.component';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,13 +12,35 @@ import { NavbarMobileComponent } from '../navbar-mobile/navbar-mobile.component'
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private scrollSpyService: ScrollSpyService) { }
+  constructor(private scrollSpyService: ScrollSpyService, private router: Router,) {
+    this.handleNavLinks();
+  }
 
   ngAfterViewInit() {
     this.scrollSpyService.spy({ thresholdBottom: 50 });
     this.scrollSpyService.activeSpyTarget.subscribe(
       (activeTargetName: string) => activeTargetName !== null ? this.setActiveNavigationTarget(activeTargetName) : ''
     );
+  }
+
+  /**
+   * Subscribes to the end of an navigation event
+   * Individual actions per route
+   */
+  handleNavLinks() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        let url = this.router.url;
+
+        // Reset active nav links on legal information page
+        if (url.includes('legal-information')) {
+          document.getElementById('nav-link-contact')?.classList.remove('nav-link-active');
+          document.getElementById('nav-link-home')?.classList.remove('nav-link-active');
+          document.getElementById('nav-link-about-me')?.classList.remove('nav-link-active');
+          document.getElementById('nav-link-my-work')?.classList.remove('nav-link-active');
+        }
+      }
+    });
   }
 
   /**

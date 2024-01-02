ba8d3e5ff52ece5113288370e727788d22705bc3
Lukas Volgger <lukas.volgger@live.at>
2024-01-02 16:58:12 +0100

    feat: use mat-chips for project tags

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatChipsModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })

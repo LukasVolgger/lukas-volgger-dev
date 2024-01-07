42a27f6ced6e10681dfffbdc6877c2e4c392246f
Lukas Volgger <lukas.volgger@live.at>
2024-01-07 14:55:18 +0100

    fix: warning related to HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withInMemoryScrolling({ anchorScrolling: "enabled" })), provideClientHydration(), provideAnimations()]
+  providers: [provideRouter(routes, withInMemoryScrolling({ anchorScrolling: "enabled" })), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch())]
 };
